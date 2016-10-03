3ef6cc405dd8986f98aa2507d077ec90b71ef3dd
niroshannrsh <niroshanrd.13@cse.mrt.ac.lk>
2016-10-03 09:02:39 +0530

    added simple auth page and framework client

--- delivery/gulpfile.js
+++ delivery/gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var webserver = require('gulp-server-livereload');
 
 gulp.task('server', function() {
-  gulp.src('./app')
+  gulp.src('./table')
     .pipe(webserver({
       livereload: true,
       open: true,
--- home/homeHandler.js
+++ home/homeHandler.js
@@ -4,7 +4,7 @@ var db = require('./lib/database')
 
 module.exports.home = function(event, context) {
 
-    page.renderHTML("https://s3.amazonaws.com/serverless-delivery-framework/", db.getUserVersion('nuwan'),context);
+    page.renderHTML("https://s3.amazonaws.com/serverless-delivery-framework/", "v2",context);
 /*
       if (event.headers.Authorization == undefined && event.query.user == undefined) {
           context.fail(("https://s3.amazonaws.com/serverless-delivery-framework/");
