3f9ca20c5cfe13fca512b37b547a007edad33b64
niroshannrsh <niroshanrd.13@cse.mrt.ac.lk>
2016-10-03 11:28:21 +0530

    added gulp commad for framweork client server

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,21 @@
 
 var gulp = require('gulp'),
     shell = require('gulp-shell'),
-    args = require('yargs').argv;
-
-
-
+    args = require('yargs').argv,
+    webserver = require('gulp-server-livereload');
 
 gulp.task('deploy-webclient', function() {
     return shell.task([
         'aws s3 sync ./build/ s3://serverless-delivery-framework/' + args.version + ' --region us-east-1'
     ])
-})
+});
 
-
-
-
-
-
-
-
-
-
-
-/
+gulp.task('server', function() {
+    gulp.src('./delivery')
+        .pipe(webserver({
+            livereload: true,
+            open: true,
+            port: 8080,
+            defaultFile: 'index.html'
+        }));
+});
--- home/homeHandler.js
+++ home/homeHandler.js
@@ -1,15 +1,14 @@
-
 var page = require('./lib/index.html')
 var db = require('./lib/database')
 
 module.exports.home = function(event, context) {
 
-    page.renderHTML("https://s3.amazonaws.com/serverless-delivery-framework/", "v2",context);
-/*
-      if (event.headers.Authorization == undefined && event.query.user == undefined) {
-          context.fail(("https://s3.amazonaws.com/serverless-delivery-framework/");
-      } else {
-        context.succeed("success");
-      }
-*/
-};
+        if (event.headers.Authorization == undefined && event.query.user == undefined) {
+
+            context.fail(("https://s3.amazonaws.com/serverless-delivery-framework/");
+            }
+            else {
+                context.succeed(db.getUserVersion("1234"));
+            }
+
+        };
